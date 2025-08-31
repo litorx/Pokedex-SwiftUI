f1719421279d06f986a38be3677fab49093a6dad
Vitor souza <113396211+litorx@users.noreply.github.com>
2025-08-30 23:32:11 -0300

    Update PokemonForm.swift

--- PokemonHome/Model/PokemonForm.swift
+++ PokemonHome/Model/PokemonForm.swift
@@ -1,17 +1,8 @@
 //
 //  PokemonForm.swift
-//  Aula1
 //
 //  Created by Vitor on 26/08/25.
 //
-
-//"forms": [
-//    {
-//        "name": "bulbasaur",
-//        "url": "https://pokeapi.co/api/v2/pokemon-form/1/"
-//    }
-//],
-
 
 
 import Foundation
